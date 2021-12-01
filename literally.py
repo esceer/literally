e81b7fd99118a625ec3f5f5377c52af6b3c68a16
Roland Kutas <roland.kutas@siemens.com>
2021-12-01 21:28:08 +0100

    Add german language support

--- literally.py
+++ literally.py
@@ -2,13 +2,18 @@ import os
 import random
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 dictionaries_dir = os.path.join(project_dir, "dictionaries")
-dictionary = os.path.join(dictionaries_dir, "korean.txt")
 
 app = Flask(__name__)
+
+
+@app.get("/")
+def root():
+    return redirect("/literally")
 
 
 @app.get("/literally")
@@ -16,16 +21,23 @@ def home():
     return render_template("home.html")
 
 
-@app.get("/literally/korean")
-def draw_word():
-    words = {}
-    with open(dictionary, "r", encoding="utf8") as f:
+@app.get("/literally/<language>")
+def draw_word(language):
+    if language not in ("german", "korean"):
+        return "The language '{}' is not supported".format(language)
+    dictionary = load_dictionary(language)
+    translation, drawn_word = random.choice(list(dictionary.items()))
+    return render_template("quiz.html", drawn_word=drawn_word, translation=translation, language=language)
+
+
+def load_dictionary(language):
+    dictionary = {}
+    input_file = os.path.join(dictionaries_dir, "{}.txt".format(language))
+    with open(input_file, "r", encoding="utf8") as f:
         for line in f:
             key, value = map(str.strip, line.split(","))
-            words[key] = value
-
-    translation, drawn_word = random.choice(list(words.items()))
-    return render_template("home.html", drawn_word=drawn_word, translation=translation)
+            dictionary[key] = value
+    return dictionary
 
 
 if __name__ == "__main__":
